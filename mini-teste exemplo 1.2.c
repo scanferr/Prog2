bd452db16a6e5760b341f0fdf7ddd185853b0b1d
scanferr <scan_ferr_mohaa@hotmail.com>
2018-03-17 17:13:23 +0000

    1.1 bom, 1.2 po crl que o foda, 1.3 bom

--- mini-teste exemplo 1.2.c
+++ mini-teste exemplo 1.2.c
@@ -1,3 +1,34 @@
+/*** problema 1.1 ***/
+lista* encontra_nomes(lista *lst1, lista *lst2)
+{
+
+		l_elemento *nome1, *nome2;
+		int i=0, j=0;
+		lista *aux = lista_nova();
+
+		if(lst1 == NULL || lst2 == NULL)
+		{
+
+			return NULL;
+		}
+
+		for (i=0; i < lst1->tamanho; i++)
+		{
+			nome1 = lista_elemento(lst1, i);
+
+			for (j = 0; j < lst2->tamanho; j++)
+			{
+				nome2 = lista_elemento(lst2, j);
+
+				if (strcmp(nome1->str, nome2->str) == 0)
+				{
+					lista_insere(aux, nome1->str, NULL);
+				}
+			}
+		}
+		return aux;
+}
+
 /*** problema 1.2 ***/
 int lista_remove_longos(lista *lst, int nomes)
 {
@@ -29,4 +60,24 @@ int lista_remove_longos(lista *lst, int nomes)
 		}
 	}
 	return n;
+}
+
+/*** problema 1.3 ***/
+fila* pesquisa_nomes(lista *lst, char *nome)
+{
+	int i;
+	l_elemento *temp;
+	fila *new = fila_nova();
+
+	if (lst == NULL || nome == NULL || lst->tamanho ==0)
+		return NULL;
+
+	for(i=0;i<lst->tamanho;i++)
+	{
+		temp=lista_elemento(lst,i);
+
+		if(strstr(temp->str,nome)!=NULL)
+			fila_push(new,temp->str);
+	}
+	return new;
 }
